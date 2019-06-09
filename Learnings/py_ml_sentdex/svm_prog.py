7bb8c74533c38f9039392345325bc11543c7b749
Loganathan S <loganathan001@gmail.com>
2019-06-09 13:00:53 +0530

    Correction in svm prog.

--- Learnings/py_ml_sentdex/svm_prog.py
+++ Learnings/py_ml_sentdex/svm_prog.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlip import style
+from matplotlib import style
 
 import numpy as np
 
@@ -31,7 +31,7 @@ class Support_Vector_Machine:
         for yi in self.data:
             for featureset in self.data[yi]:
                 for feature in featureset:
-                    all_data.append(featureset)
+                    all_data.append(feature)
 
         self.max_feature_value = max(all_data)
         self.min_feature_value = min(all_data)
@@ -47,7 +47,7 @@ class Support_Vector_Machine:
                       self.max_feature_value * 0.001]
 
         # extremely expensive
-        b_range_multiple = 5
+        b_range_multiple = 2
         # we don't need to take as small of steps
         # with b as we do w
         b_multiple = 5
@@ -59,8 +59,8 @@ class Support_Vector_Machine:
             # we can do this because convex
             optimized=False
             while not optimized:
-                for b in np.arange(-1 * (max_feature_value*b_range_multiple),
-                                   max_feature_value*b_range_multiple,
+                for b in np.arange(-1 * (self.max_feature_value*b_range_multiple),
+                                   self.max_feature_value*b_range_multiple,
                                    step*b_multiple):
                     for transformation in transforms:
                         w_t = w*transformation
@@ -72,7 +72,8 @@ class Support_Vector_Machine:
                             for xi in self.data[i]:
                                 yi=i
                                 if not yi*(np.dot(w_t,xi) + b) >= 1:
-                                    found_option=True
+                                    found_option=False
+                                #print(xi, ":", yi*(np.dot(w_t,xi) + b))
 
                         if found_option:
                             opt_dict[np.linalg.norm(w_t)] = [w_t,b]
@@ -85,10 +86,15 @@ class Support_Vector_Machine:
                 
             norms = sorted([n for n in opt_dict])
             # Magnitude: ||w|| = [w,b]
-            opt_choice = opt_dict(norms[0])
+            opt_choice = opt_dict[norms[0]]
             self.w = opt_choice[0]
             self.b = opt_choice[1]
             latest_optimum = opt_choice[0][0]+step*2
+
+        for i in self.data:
+            for xi in self.data[i]:
+                yi=i
+                print(xi, ':', yi*(np.dot(self.w, xi)+self.b) )
 
             
                                 
@@ -99,7 +105,46 @@ class Support_Vector_Machine:
     def predict(self, features):
         # sign( x.w + b )
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+
+        if classification!=0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
+        
         return classification
+
+
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane x.w + b - Just for visualizing the hyperplane
+        # here v = x.w + b
+        # v for positive sv = 1, negative sv = -1, decision boundary = 0
+        def hyperplane(x,w,b,v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value*0.9,self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # w.x+b = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max],[psv1,psv2], 'k')
+
+        # w.x+b = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max],[nsv1,nsv2], 'k')
+
+        # w.x+b = 0
+        # decision boundary support vector hyperplane
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max],[db1,db2], 'y--')
+
+        plt.show()
+
         
         
 
@@ -108,8 +153,23 @@ data_dict = {-1:np.array([[1,7],
                           [3,8]]),
              
              1:np.array([[5,1],
-                         [5,1],
                          [6,-1],
                          [7,3]])}
 
+predict_us = [[0,10],
+              [1,3],
+              [3,4],
+              [3,5],
+              [5,5],
+              [5,6],
+              [6,-5],
+              [5,8]]
 
+
+svm = Support_Vector_Machine()
+svm.fit(data=data_dict)
+
+for p in predict_us:
+    svm.predict(p)
+
+svm.visualize()

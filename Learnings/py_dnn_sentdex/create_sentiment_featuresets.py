4025c2f2710c876b20fc8f8fb040d9c50770b33e
Loganathan S <loganathan001@gmail.com>
2019-06-26 06:46:53 +0530

    Adding dnn

---
+++ Learnings/py_dnn_sentdex/create_sentiment_featuresets.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+import  pickle
+from collections import Counter
+
+lemmatizer = WordNetLemmatizer()
+hm_lines = 
+
+
